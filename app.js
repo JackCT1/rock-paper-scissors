8b442dbf27f0f30503049621b382b68434f0c4c0
JackCT1 <jackthomas933@gmail.com>
2023-05-16 16:19:40 +0100

    basic result function working

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const numberToChoice = { 1: "Rock", 2: "Paper", 3: "Scissors" };
+const possibleChoices = ["Rock", "Paper", "Scissors"];
 
 const computerChoice = document.getElementById("computer-choice");
 const userChoice = document.getElementById("user-choice");
 const result = document.getElementById("result");
-const possibleChoices = document.querySelectorAll("button");
+const choiceButtons = document.querySelectorAll("button");
 let userButtonSelect;
 
-possibleChoices.forEach((choice) =>
+choiceButtons.forEach((choice) =>
   choice.addEventListener("click", (e) => {
     userButtonSelect = e.target.id;
     userChoice.innerHTML = userButtonSelect;
     computerChoice.innerHTML = generateComputerChoice();
+    result.innerHTML = getResult(userButtonSelect, computerChoice.innerHTML);
   })
 );
 
 function generateComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
-  let computerSelect = numberToChoice[randomNumber];
+  let randomNumber = Math.floor(Math.random() * choiceButtons.length);
+  let computerSelect = possibleChoices[randomNumber];
   return computerSelect;
+}
+
+function getResult(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "Draw!";
+  } else if (
+    (userChoice === "Rock" && computerChoice === "Scissors") ||
+    (userChoice === "Paper" && computerChoice === "Rock") ||
+    (userChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    return "User Wins!";
+  }
+  return "Computer Wins!";
 }
